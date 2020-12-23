70198fdc0c4a7faa7a5cf61b7c878dd3d955bdaf
iamthankyou <quangduynb00@gmail.com>
2020-12-23 12:41:45 +0700

    Analysic codeforces

--- src/Contest690/ECloseTuplesAddToNeighborAndRemove.py
+++ src/Contest690/ECloseTuplesAddToNeighborAndRemove.py
@@ -1,23 +1,34 @@
+import collections
 def solve():
     n = int(input())
     arr = list(map(int,input().split()))
-    total = sum(arr)
 
-    for i in range(n,0,-1):
-        if total%i==0:
-            sumBalance = total//i
-            sumCurr = 0
-            flag = True
-            for j in range(n):
-                sumCurr+=arr[j]
-                if sumCurr>sumBalance:
-                    flag=False
-                    break
-                elif sumCurr==sumBalance:
-                    sumCurr=0
-            if flag==True:
-                print(n-i)
-                return
+    cnt = [0]*200005
+
+    for i in range(len(arr)):
+        cnt[arr[i]]+=1
+
+    ans = 0
+
+    for i in range(200000):
+        ans+=(cnt[i]-1)*(cnt[i]-2)*cnt[i]//6
+
+    for i in range(200000):
+        ans+=cnt[i]*cnt[i+1]*cnt[i+2]
+
+    for i in range(200000):
+        ans+=cnt[i]*(cnt[i+1]-1)*cnt[i+1]//2
+
+    for i in range(200000):
+        ans+=(cnt[i]-1)*cnt[i]//2*cnt[i+1]
+
+    for i in range(200000):
+        ans+=cnt[i]*(cnt[i+2]-1)*cnt[i+2]//2
+
+    for i in range(200000):
+        ans+=(cnt[i]-1)*cnt[i]//2*cnt[i+2]
+
+    print(ans)
 
 if __name__ == '__main__':
     t = int(input())
