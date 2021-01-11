2fa605f7056e578ac08aaca9f04faf945b90a948
Le Quang Duy <quangduynb00@gmail.com>
2021-01-12 01:57:34 +0700

    A problem with codeforces

--- src/Contest695Div2/A.cpp
+++ src/Contest695Div2/A.cpp
@@ -1,7 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
+void solve(){
+    int n; cin >> n;
+    string ans = "989";
+ 
+    if (n<3){
+        cout << ans.substr(0,n) << "\n";
+        return;
+    }
+    
+    cout << ans;
+ 
+    for (int i=3; i<n; i++){
+        cout << (i-3)%10;
+    }
+ 
+    cout << "\n";
+}
+ 
 int main(){
-    freopen("duy.out","w",stdout);
-    cout << "Hello Duy";
+    // freopen("duy.in","r",stdin);
+    // freopen("duy.out","w",stdout);
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t; cin >> t;
+ 
+    while (t--){
+        solve();
+    }
 }
